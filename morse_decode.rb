89eccd0084cb33c9f5804bd903fa3ca61ae34703
Ismail mayito <ismayito1@gmail.com>
2023-11-09 17:06:53 +0300

    Fix linter errors

--- morse_decode.rb
+++ morse_decode.rb
@@ -1,31 +1,29 @@
 MORSE_CODES = {
-    '.-' => 'A', '-...' => 'B', '-.-.' => 'C', '-..' => 'D', '.' => 'E',
-    '..-.' => 'F', '--.' => 'G', '....' => 'H', '..' => 'I', '.---' => 'J',
-    '-.-' => 'K', '.-..' => 'L', '--' => 'M', '-.' => 'N', '---' => 'O',
-    '.--.' => 'P', '--.-' => 'Q', '.-.' => 'R', '...' => 'S', '-' => 'T',
-    '..-' => 'U', '...-' => 'V', '.--' => 'W', '-..-' => 'X', '-.--' => 'Y',
-    '--..' => 'Z',
-    ' ' => ' '
+  '.-' => 'A', '-...' => 'B', '-.-.' => 'C', '-..' => 'D', '.' => 'E',
+  '..-.' => 'F', '--.' => 'G', '....' => 'H', '..' => 'I', '.---' => 'J',
+  '-.-' => 'K', '.-..' => 'L', '--' => 'M', '-.' => 'N', '---' => 'O',
+  '.--.' => 'P', '--.-' => 'Q', '.-.' => 'R', '...' => 'S', '-' => 'T',
+  '..-' => 'U', '...-' => 'V', '.--' => 'W', '-..-' => 'X', '-.--' => 'Y',
+  '--..' => 'Z',
+  ' ' => ' '
 }.freeze
 
 # Decoding letter
 def decode_string(morse_string)
-    MORSE_CODES[morse_string] || ''
+  MORSE_CODES[morse_string] || ''
 end
 
-morse_code1 = ".-"
+morse_code1 = '.-'
 decoded_string = decode_string(morse_code1)
 puts decoded_string
 
-
 # Decoding word
 def decode_word(morse_word)
-    morse_word.split.map { |morse_string| decode_string(morse_string) }.join
+  morse_word.split.map { |morse_string| decode_string(morse_string) }.join
 end
 
-morse_code2 = "-- -.--"
+morse_code2 = '-- -.--'
 decoded_word = decode_word(morse_code2)
 puts decoded_word
-
 
 # Decoding message
