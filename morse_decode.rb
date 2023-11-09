3f040d0003ef5c42ac6f0c19787518b63774e6bc
Shinn Thant Swam Ye <1shinnthantswamye@gmail.com>
2023-11-09 19:13:33 +0630

    Add decoding methods for letter and word

---
+++ morse_decode.rb
@@ -0,0 +1,31 @@
+MORSE_CODES = {
+    '.-' => 'A', '-...' => 'B', '-.-.' => 'C', '-..' => 'D', '.' => 'E',
+    '..-.' => 'F', '--.' => 'G', '....' => 'H', '..' => 'I', '.---' => 'J',
+    '-.-' => 'K', '.-..' => 'L', '--' => 'M', '-.' => 'N', '---' => 'O',
+    '.--.' => 'P', '--.-' => 'Q', '.-.' => 'R', '...' => 'S', '-' => 'T',
+    '..-' => 'U', '...-' => 'V', '.--' => 'W', '-..-' => 'X', '-.--' => 'Y',
+    '--..' => 'Z',
+    ' ' => ' '
+}.freeze
+
+# Decoding letter
+def decode_string(morse_string)
+    MORSE_CODES[morse_string] || ''
+end
+
+morse_code1 = ".-"
+decoded_string = decode_string(morse_code1)
+puts decoded_message
+
+
+# Decoding word
+def decode_word(morse_word)
+    morse_word.split.map { |morse_string| decode_string(morse_string) }.join
+end
+
+morse_code2 = "-- -.--"
+decoded_word = decode_word(morse_code2)
+puts decoded_word
+
+
+# Decoding message
